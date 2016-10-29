727e5ed166ac46c45d81ce4d530e9e9d32e2f36e
alphayax <alphayax@gmail.com>
2016-10-29 16:10:51 +0200

    Back end : Implement cache for config

--- src/etc/Config.php
+++ src/etc/Config.php
@@ -9,17 +9,35 @@ class Config {
 
     const CONFIG_DIRECTORY = __DIR__;
 
+    /** @var array */
+    protected static $configsCache = [];
+
     /**
      * @param $confName
      * @return array
      */
     public static function get( $confName) {
-        $configFile = static::CONFIG_DIRECTORY . DIRECTORY_SEPARATOR . $confName . '.json';
-        if( file_exists( $configFile)){
-            $confContent = file_get_contents( $configFile);
-            return json_decode( $confContent, true);
+
+        if( ! array_key_exists( $confName, static::$configsCache)){
+            static::load( $confName);
         }
-        return [];
+
+        return static::$configsCache[$confName];
+    }
+
+    /**
+     * @param $confName
+     */
+    protected static function load( $confName) {
+        $configFile = static::getConfigFileFromConfigName( $confName);
+
+        if( ! file_exists( $configFile)){
+            static::$configsCache[$confName] = [];
+            return;
+        }
+
+        $confContent = file_get_contents( $configFile);
+        static::$configsCache[$confName] = json_decode( $confContent, true);
     }
 
     /**
@@ -28,9 +46,29 @@ class Config {
      * @return bool
      */
     public static function set( $confName, $confContent) {
-        $configFile = static::CONFIG_DIRECTORY . DIRECTORY_SEPARATOR . $confName . '.json';
-        $confContent = json_encode( $confContent);
-        return file_put_contents( $configFile, $confContent) != false;
+
+        static::$configsCache[$confName] = $confContent;
+
+        return static::write( $confName);
+    }
+
+    /**
+     * @param $confName
+     * @return bool
+     */
+    protected static function write( $confName) {
+        $configFile = static::getConfigFileFromConfigName( $confName);
+
+        $configContent = json_encode( static::$configsCache[$confName]);
+        return false !== file_put_contents( $configFile, $configContent);
+    }
+
+    /**
+     * @param $confName
+     * @return string
+     */
+    protected static function getConfigFileFromConfigName( $confName) {
+        return static::CONFIG_DIRECTORY . DIRECTORY_SEPARATOR . $confName . '.json';
     }
 
 }
